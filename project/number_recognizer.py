ce852d192c79aabec0902b48c7097e77a5d6e5f6
jsm820 <sangmukjo@gmail.com>
2014-04-25 23:34:01 +0900

    number recognizer

--- project/number_recognizer.py
+++ project/number_recognizer.py
@@ -1,30 +1,66 @@
+from bsddb.dbtables import ExactCond
 from flask import Flask
 from flask import request
 from os import listdir
+from numpy import *
+import operator
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
 
-@app.route('/number/<name>', methods=['GET', 'POST'])
-def home(name):
-    f = open('file.txt','w')
-    a = request.args.get('key', '')
-    f.write('answer:'+str(a))
-    f.close()
+@app.route('/hello')
+def hello():
+    return 'hello'
 
-    if(name == 'save'):
-        number = request.args.get('number', -1)
-        numberString = request.args.get('result', '');
+@app.route('/number/<name>',  methods=['POST', 'GET'])
+def home(name):
+    if name == 'save':
+        if request.method == 'POST':
+            number = request.form['number']
+            numberString = request.form['result']
+        else:
+            number = request.args.get('number', -1)
+            numberString = request.args.get('result', '')
         saveNumber(number, numberString)
 
-    return  'hello'
+    elif name == 'classify':
+        if request.method == 'POST':
+            numberString = request.form('result')
+        else:
+            numberString = request.args.get('result', '')
+        classify(numberString)
+
+    return 'hello'
+
+def stringToArray(string):
+    stringSize = len(string)
+    width = 1
+
+    while width * width < stringSize:
+        width += 1
+
+    stringArray = []
+    for i in range(width):
+        stringArray.append(string[i*width:(i+1)*width])
+    return width, stringArray;
+
+def stringToVector(width, stringArray):
+    returnVect = zeros((1,width * width))
+    for i in range(width):
+        for j in range(width):
+            returnVect[0,width*i+j] = int(stringArray[j])
+
+    return width, returnVect
 
 def saveNumber(number, numberString):
     count = 0
     trainingFileList = listdir('trainingSet')          
     m = len(trainingFileList)
+
+    width, stringVector = stringToArray(numberString)
 
     for i in range(m):
         fileNameStr = trainingFileList[i]
@@ -35,8 +71,83 @@ def saveNumber(number, numberString):
             count += 1
 
     f = open('trainingSet/' + number + '_' + str(count) + '.txt','w')
-    f.write(numberString)
+
+    for i in range(len(stringVector)):
+        f.write(stringVector[i]+'\n')
+
     f.close()
+
+def classify(numberString):
+    width, stringArray = stringToArray(numberString)
+    width, stringVector = stringToVector(width, stringArray)
+
+    hwLabels = []
+    trainingFileList = listdir('trainingSet')           #load the training set
+    m = len(trainingFileList)
+    trainingMat = zeros((m,width * width))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingSet/%s' % fileNameStr, width)
+
+    classifierResult = classify0(stringVector, trainingMat, hwLabels, 3)
+
+    for i in len(classifierResult):
+        print classifierResult[i][0]
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount={}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount
+
+def file2matrix(filename):
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())         #get the number of lines in the file
+    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
+    classLabelVector = []                       #prepare labels return
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
+    return normDataSet, ranges, minVals
+
+def img2vector(filename, width):
+    returnVect = zeros((1,width*width))
+    fr = open(filename)
+    for i in range(width):
+        lineStr = fr.readline()
+        for j in range(width):
+            try:
+                returnVect[0,width*i+j] = int(lineStr[j])
+            except Exception, e:
+                returnVect[0,width*i+j] = 0
+
+    return returnVect
 
 
 if __name__ == '__main__':
